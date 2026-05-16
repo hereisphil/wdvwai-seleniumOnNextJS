91d2602fde3736d856bc3bb745fbc71fdf0dfb54
hereisphil <thereisphil@gmail.com>
2026-05-16 13:31:13 -0600

    Updated chromedrive, installed jest and added to demoExternalTest script, removed redudnant require line in external.test.js and wrapped functional in a Jest test call, and lastly removed the 'demoTest' script that referenced a nonexistent file

--- external.test.js
+++ external.test.js
@@ -1,12 +1,13 @@
-require('chromedriver');
 const { Builder, By, Key, until } = require('selenium-webdriver')
 
-const driver = new Builder().forBrowser('chrome').build()
+describe("", ()=>{
+  const driver = new Builder().forBrowser('chrome').build()
 
-driver
-  .get('http://www.google.com/ncr')
-  .then((_) =>
-    driver.findElement(By.name('q')).sendKeys('nicholas cage', Key.RETURN)
-  )
-  .then((_) => driver.wait(until.titleIs('nicholas cage - Google Search'), 1000))
-  .then((_) => driver.quit())
+  driver
+    .get('http://www.google.com/ncr')
+    .then((_) =>
+      driver.findElement(By.name('q')).sendKeys('nicholas cage', Key.RETURN)
+    )
+    .then((_) => driver.wait(until.titleIs('nicholas cage - Google Search'), 1000))
+    .then((_) => driver.quit())
+})
